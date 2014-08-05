c2e2a8e50c50bb43e3fe5390fde2f7317aba479f
Steven Deeds <stevendeeds@Stevens-Mac-mini.local>
2014-08-05 16:17:38 -0500

    got setting loading in right, got camera working on adding posts, everything works, except for submitting preferences. gotta do that next

--- platforms/ios/www/js/app.js
+++ platforms/ios/www/js/app.js
@@ -1,25 +1,5 @@
 /****** PHONEGAP INIT *******/
-var app = {
-    initialize: function() {
-        this.bindEvents();
-    },
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-    },
-    onDeviceReady: function() {
-        app.receivedEvent('deviceready');
-    },
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
 
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-
-        console.log('Received Event: ' + id);
-    }
-};
 /****************************/
 
 
@@ -44,11 +24,8 @@ var app = {
 
 
 /******* PHONEGAP FUNCTIONS *************/
-
-	function locateMe() {
-	    var options = { timeout: 30000 };
-	    watchID = navigator.geolocation.getCurrentPosition(onGeoSuccess, onGeoError, options);
-	}
+	/****** GEO LOCATION ********/
+	
 	
 	function onGeoSuccess(position) {
 	    $("input[name='lat']").val(position.coords.latitude);
@@ -61,9 +38,112 @@ var app = {
 	    console.log('code: '    + error.code    + '\n' +
 	          'message: ' + error.message + '\n');
 	}
+	function locateMe() {
+	    var options = { timeout: 30000 };
+	    watchID = navigator.geolocation.getCurrentPosition(onGeoSuccess, onGeoError, options);
+	}
+	locateMe();
 	
 	
 	
+	
+	
+	/****** CAMERA STUFF *********/
+
+		
+		
+	    var pictureSource;   // picture source
+	    var destinationType; // sets the format of returned value 
+	
+	    // Wait for Cordova to connect with the device
+	    //
+	    document.addEventListener("deviceready",onDeviceReady,false);
+	
+	    // Cordova is ready to be used!
+	    //
+	    function onDeviceReady() {
+	        pictureSource=navigator.camera.PictureSourceType;
+	        destinationType=navigator.camera.DestinationType;
+	    }
+	
+	    // Called when a photo is successfully retrieved
+	    //
+	    function onPhotoDataSuccess(imageData) {
+	      // Uncomment to view the base64 encoded image data
+	      // console.log(imageData);
+	
+	      // Get image handle
+	      //
+	      var smallImage = document.getElementById('smallImage');
+	      var photo_data_input = $("input[name='post_data']");
+	
+	      // Unhide image elements
+	      //
+	      smallImage.style.display = 'block';
+	
+	      // Show the captured photo
+	      // The inline CSS rules are used to resize the image
+	      //
+	      smallImage.src = "data:image/jpeg;base64," + imageData;
+	      photo_data_input.val("data:image/jpeg;base64," + imageData);
+	    }
+	
+	    // Called when a photo is successfully retrieved
+	    //
+	    function onPhotoURISuccess(imageURI) {
+	      // Uncomment to view the image file URI 
+	      // console.log(imageURI);
+	
+	      // Get image handle
+	      //
+	      var largeImage = document.getElementById('largeImage');
+	
+	      // Unhide image elements
+	      //
+	      largeImage.style.display = 'block';
+	
+	      // Show the captured photo
+	      // The inline CSS rules are used to resize the image
+	      //
+	      largeImage.src = imageURI;
+	    }
+	
+	    // A button will call this function
+	    //
+	    function capturePhoto() {
+	      // Take picture using device camera and retrieve image as base64-encoded string
+	      navigator.camera.getPicture(onPhotoDataSuccess, onFail, { 
+	      	quality: 50,
+	      	targetWidth: 400,
+		  	targetHeight: 400,
+	        destinationType: navigator.camera.DestinationType.DATA_URL
+	       });
+	    }
+	
+	    // A button will call this function
+	    //
+	    function capturePhotoEdit() {
+	      // Take picture using device camera, allow edit, and retrieve image as base64-encoded string  
+	      navigator.camera.getPicture(onPhotoDataSuccess, onFail, { quality: 20, allowEdit: true,
+	        destinationType: navigator.camera.DestinationType.DATA_URL });
+	    }
+	
+	    // A button will call this function
+	    //
+	    function getPhoto(source) {
+	      // Retrieve image file location from specified source
+	      navigator.camera.getPicture(onPhotoURISuccess, onFail, { quality: 50, 
+	        destinationType: navigator.camera.DestinationType.FILE_URI,
+	        sourceType: source });
+	    }
+	
+	    // Called if something bad happens.
+	    // 
+	    function onFail(message) {
+	      alert('Failed because: ' + message);
+	    }
+
+
 
 
 /***************************************/
@@ -114,16 +194,13 @@ function changePage(choice, formdata) {
 					data 		: formdata, 
 					dataType 	: 'json',
 					success 	: function(result) {
-/* 						$("#alerts").html(JSON.stringify(result)); */
 						$("nav a").removeClass("active");
-						$("a[name='"+choice+"']").addClass("active");	
-/* 						$("#alerts").html(JSON.stringify(formdata)); */
+						$("nav a[name='"+choice+"']").addClass("active");	
 						var templateSource = $("#"+choice).html();
 					    var compiledTemplate = Handlebars.compile(templateSource);
 				   		$("#content-placeholder").html(compiledTemplate(result));
-					   		
 					}, 
-					error 		: function() {
+					error 		: function(result) {
 						$("#alerts").html(JSON.stringify(result));
 						
 					}
@@ -131,7 +208,7 @@ function changePage(choice, formdata) {
 			}); 
 
 }
-locateMe();
+
 $(document).ready(function() {
 	if(! localStorage.getItem("userToken")) {
 		/*********************************/
@@ -149,19 +226,6 @@ $(document).ready(function() {
 });
 
 
-/*changePage(choice, function() {});
-changePage(choice, function(obj) {
-	//bla
-});
-
-var myFunc = function() {//do stuff
-};
-function myFunc() {}
-
-changePage("home", myFunc);	
-*/	
-
-
 	/*********** CHANGE TEMPLATE ***********/
 	$("nav a").click(function() {
 		locateMe();
@@ -172,6 +236,8 @@ changePage("home", myFunc);
 		    var choice = $(this).attr("name");
 		    $("input[name='request']").val(choice);
 			if(choice === "new_post") {
+				$("nav a").removeClass("active");
+				$("nav a[name='"+choice+"']").addClass("active");	
 				var templateSource = $("#"+choice).html();
 			    var compiledTemplate = Handlebars.compile(templateSource);
 		   		$("#content-placeholder").html(compiledTemplate());
@@ -181,6 +247,46 @@ changePage("home", myFunc);
 			}
 	   		
 	});	
+	
+	
+	
+	
+	
+	
+	
+	
+/************ TAP ACTIONS ******************/	
+	
+	$('#content-placeholder').on('click', '.big_button a', function() {
+	    capturePhotoEdit();
+	});
+	$('#content-placeholder').on('click', '#new_post_submit', function() {
+		var formdata = formToJSON("#form");
+	    $.ajax({ 
+				type 		: 'POST', 
+				url 		: 'http://gobeebot.com/',
+				data 		: formdata, 
+				dataType 	: 'json',
+				success 	: function(data) {
+					if (!data.success) { //If fails
+						console.log("error");
+						if (data.errors) { 
+							console.log(data.errors);
+						}
+					} 
+					else {
+						console.log("sent");
+						console.log(JSON.stringify(data));
+						$("#smallImage").css({"display" : "none"});
+					}
+				}, 
+				error 		: function(result) {
+					$("#alerts").html(JSON.stringify(result));
+					
+				}
+	
+		}); 
+	});
 	
 	
 /**************************************/
